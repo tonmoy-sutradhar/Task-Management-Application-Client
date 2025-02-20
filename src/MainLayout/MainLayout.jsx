284b3b0d47f73c8cd32b5a499ab8553cc864e026
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-02-20 22:37:18 +0600

    Firebase setup done.

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -2,15 +2,30 @@ import React from "react";
 import Navbar from "../Component/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Footer from "../Component/Footer/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar></Navbar>
-      <div>
+      <div className="min-h-[calc(100vh-359px)] py-2 container mx-auto ">
         <Outlet></Outlet>
       </div>
       <Footer></Footer>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 };
