2a9a3f23a9ac1031fb802d077df31b5469431205
Tonmoy <tonmoysutradhar2002@gmail.com>
2025-02-20 23:12:22 +0600

    AuthProvider setup done.

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
+
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { logOut } = useAuth();
+  useEffect(() => {
+    axiosSecure.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      async (error) => {
+        console.log("Error caught from axios interceptor-->", error.response);
+        if (error.response.status === 401 || error.response.status === 403) {
+          logOut();
+          navigate("/login");
+          // console.log("hello");
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, [logOut, navigate]);
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
